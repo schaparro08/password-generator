f588eb788ef59807a0285acd5d4d8f9ed0d81380
Stephanie <101684009+schaparro08@users.noreply.github.com>
2022-03-30 17:20:03 -0400

    creation of prompt How many characters would you like the password to contain

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+// Arrays for special characters, lowercase letters, uppercase letters, and numbers
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}",
+ "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
+
+const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+ "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+ "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+ //prompt how many characters we would like the Pw to contain. Must be at least 8 but no more than 128
+var generatePassword = function() {
+  //ask user for number of characters
+    var userchoice = prompt("How many characters would you like the password to contain");
+    if (!userchoice) {
+      return;
+    }
+    else if (userchoice)
+}
+
+ //Do you want special characters
+
+ //Do you want numbers
+
+ // Do you want to include lowercase characters
+
+
+ // do you want to contain uppercase characters
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
